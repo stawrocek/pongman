417c6dc99492571e70c6a190aeb9c6d12c90bb28
Stanisław Koza <stasiukoza1234@gmail.com>
2015-07-10 13:50:44 +0200

    sterowanie klawiszami
    -ciut lepiej

--- Pongman/Assets/Scripts/Game/Player/MovePlayer.cs
+++ Pongman/Assets/Scripts/Game/Player/MovePlayer.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class MovePlayer : MonoBehaviour {
 
+	public float moveSpeed = 10f;
+	public float rotateSpeed = 50f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,27 @@ public class MovePlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (1 * Time.deltaTime, 0, 0);
+		/*if (Input.GetAxis ()) {
+		
+		}
+		transform.Translate (1 * Time.deltaTime, 0, 0);*/
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.Translate (0, 0, Time.deltaTime*moveSpeed);
+		}
+		else if(Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.Translate (0, 0, Time.deltaTime*moveSpeed);
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			//transform.Translate (-Time.deltaTime*moveSpeed, 0, 0);
+			transform.Rotate (0, -rotateSpeed * Time.deltaTime, 0);
+		}
+		else if(Input.GetKey(KeyCode.RightArrow))
+		{
+			//transform.Translate (Time.deltaTime*moveSpeed, 0, 0);
+			transform.Rotate (0, rotateSpeed * Time.deltaTime, 0);
+		}
 	}
 }
---
+++ Pongman/Assets/Scripts/Game/Player/RandomColor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetPlaeyRandomColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//((MeshRenderer)this.GetComponent<MeshRenderer>()).SetC
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
