7c22f101bbe49db16dd0e8bb4654dea5a472ae1e
Stanisław Koza <stasiukoza1234@gmail.com>
2015-07-10 16:27:15 +0200

    zastąpienie modułu NetworkTransform własnym
    -to co w temacie
    -możliwość zmiany koloru gracza lokalnie (klawisze 1-5)

--- Pongman/Assets/Scripts/Game/Player/MovePlayer.cs
+++ Pongman/Assets/Scripts/Game/Player/MovePlayer.cs
@@ -7,7 +7,8 @@ public class MovePlayer : MonoBehaviour {
 
 	public float moveSpeed = 10f;
 	public float rotateSpeed = 50f;
-
+	Color[] colorArray = new Color[] {Color.black, Color.white, Color.red, Color.green, Color.yellow};
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -30,12 +31,30 @@ public class MovePlayer : MonoBehaviour {
 		if(Input.GetKey(KeyCode.LeftArrow))
 		{
 			transform.Translate (-Time.deltaTime*moveSpeed, 0, 0);
-			//transform.Rotate (0, -rotateSpeed * Time.deltaTime, 0);
 		}
 		else if(Input.GetKey(KeyCode.RightArrow))
 		{
 			transform.Translate (Time.deltaTime*moveSpeed, 0, 0);
-			//transform.Rotate (0, rotateSpeed * Time.deltaTime, 0);
+		}
+		else if(Input.GetKey(KeyCode.Alpha1))
+		{
+			GetComponent<Renderer>().material.color = colorArray[0];
+		}
+		else if(Input.GetKey(KeyCode.Alpha2))
+		{
+			GetComponent<Renderer>().material.color = colorArray[1];
+		}
+		else if(Input.GetKey(KeyCode.Alpha3))
+		{
+			GetComponent<Renderer>().material.color = colorArray[2];
+		}
+		else if(Input.GetKey(KeyCode.Alpha4))
+		{
+			GetComponent<Renderer>().material.color = colorArray[3];
+		}
+		else if(Input.GetKey(KeyCode.Alpha5))
+		{
+			GetComponent<Renderer>().material.color = colorArray[4];
 		}
 	}
 }
---
+++ Pongman/Assets/Scripts/Game/Player/PlayerSyncPosition.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSyncPosition : NetworkBehaviour {
+
+	[SyncVar]
+	private Vector3 syncPos;
+
+	[SerializeField] Transform myTransform;
+	[SerializeField] float lerpRate = 15;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		TransmitPosition ();
+		lerpPosition ();
+	}
+
+	void lerpPosition()
+	{
+		if (!isLocalPlayer) {
+			myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime*lerpRate);
+		}
+	}
+
+	[Command]
+	void CmdProvidePositionToServer(Vector3 pos){
+		syncPos = pos;
+	}
+
+	[ClientCallback]
+	void TransmitPosition(){
+		if (isLocalPlayer) {
+			CmdProvidePositionToServer (myTransform.position);
+		}
+	}
+}
